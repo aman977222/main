2688fe76abf8061756d2b6a14547a2f3f282b698
aman977222 <aman.jangid1304@gmail.com>
2026-03-09 11:54:18 +0530

    index.html

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+$OPENAI_API_KEY = "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"; 
+?>
---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div class="bg-info p-3 text-center">
+    <p>reserved Designd by Aman kumer jangid(vgu24ons2bca****)</p>
+</div>
